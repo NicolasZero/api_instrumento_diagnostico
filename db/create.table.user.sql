df071d18a12d485c2d42824edb3175a45e686330
Nicolas <nicojs2011@gmail.com>
2024-11-15 00:04:12 -0400

    sql create user corregido

--- db/create.table.user.sql
+++ db/create.table.user.sql
@@ -2,8 +2,10 @@
 CREATE TABLE usuarios (
     id integer NOT NULL UNIQUE GENERATED ALWAYS AS IDENTITY (START WITH 1),
     username varchar NOT NULL UNIQUE,
+    cedula varchar NOT NULL UNIQUE,
     name varchar NOT NULL,
     last_name varchar NOT NULL,
     password varchar NOT NULL,
-    is_active boolean DEFAULT true
+    is_active boolean DEFAULT true,
+    rol varchar NOT NULL DEFAULT 'usuario'
 )
